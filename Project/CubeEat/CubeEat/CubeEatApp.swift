483d1f4278b8da63be3f048ed56524d9daa56d00
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-09-03 16:29:18 -0300

    adicionando o projeto

---
+++ Project/CubeEat/CubeEat/CubeEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeEatApp.swift
+//  CubeEat
+//
+//  Created by Jairo Júnior on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CubeEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
